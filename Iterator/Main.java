9c666364ae953334a7b7fc726b4a74c4c7c41f62
Joey Kingston <48415221+gkingston0711@users.noreply.github.com>
2021-04-01 08:04:30 -0700

    iterator3

--- Iterator/Main.java
+++ Iterator/Main.java
@@ -1,105 +1,104 @@
 package com.company;
 
-import java.util.Hashtable;
+import java.util.Vector;
 
-abstract class bulb{
-    int power;
-    String brand;
-    void emitLight() {}
+abstract class Unit{
+    String name;
+    Unit(String name){this.name=name;}
+    abstract void print();
+    /** NEEDED BELOW FOR VISITOR **/
+    abstract Object accept(VisitorIF visitor, Object data);
+
 }
 
-class LED extends bulb {
-    final int type = 1;//rating based on eco friendliness
+class Composite extends Unit {
+    Vector<Unit> members = new Vector<>();
 
-     public LED(int power,String brand){
-         this.power=power;
-         this.brand=brand;
-
-         System.out.println("LED light with lumination power of "+this.power+ " and brand "+ this.brand +" is being CREATED");
-     }
-
-    @Override
-    public void emitLight() {
-        System.out.println("LED light with a lumination power of "+this.power+" and brand is "+this.brand);
-
-    }
-}
-
-class incandescent extends bulb{
-    final int type=0;//rating based on eco friendliness
-
-    public incandescent(int power,String brand){
-        this.power=power;
-        this.brand=brand;
-
-        System.out.println("incandescent light with lumination power of "+this.power+ " and brand "+ this.brand +" is being CREATED");
+    Composite(String name) {
+        super(name);
     }
 
-    @Override
-    public void emitLight() {
-        System.out.println("Incandescent light with a lumination power of "+this.power+" and brand is "+this.brand);
-    }
-}
-
-class flyweightFactory{
-
-
-
-    Hashtable<String,bulb> table = new Hashtable<>();
-
-    bulb getBulb(int t, int p, String l){
-
-        String key = t + " " + p + " " + l;
-
-        bulb b = table.get(key);
-
-        if(b==null){
-            if(t==0){
-                b = new LED(p,l);
-            }
-            if(t==1){
-                b= new incandescent(p,l);
-            }
-            table.put(key,b);
+    void print() {
+        System.out.println("Composite Name: "+ this.name);
+        for(Unit i : members){
+            i.print();
         }
-        else{
-            System.out.println("Bulb already exists... returning it to you now");
-        }
+    }
 
-        return b;
+    void addMember(Unit unit) {
+        members.add(unit);
+    }
+    /** NEEDED BELOW FOR VISITOR **/
+    Object accept(VisitorIF visitor, Object data) {
+        return visitor.visit(this, data);
+    }
+}
+
+class Leaf extends Unit {
+    Leaf(String name) {
+        super(name);
+    }
+    void print(){
+        System.out.println("Leaf Name: "+ this.name);
+    }
+    /** NEEDED BELOW FOR VISITOR **/
+    Object accept(VisitorIF visitor, Object data) {
+        return visitor.visit(this, data);
     }
 
 }
+
+/** VISITOR BELOW **/
+
+interface VisitorIF{
+    Object visit(Leaf individual, Object data);
+    Object visit(Composite group, Object data);
+}
+
+class Visitor implements VisitorIF {
+
+    public Object visit(Leaf individual, Object data) {
+        return individual.name.equals((String) data);
+    }
+    public Object visit(Composite group, Object data) {
+        if (group.name.equals((String) data)){
+            return true;
+        }
+
+        for (Unit u : group.members) {
+            if ((Boolean) u.accept(this, data)){
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
+
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        long beforeUsedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
-
-        flyweightFactory flyweight = new flyweightFactory();
-        String[] brandArray = new String[]{ "bell","bower","hager","GE",};
-
-        long startTime = System.nanoTime();
-        for(int i =0 ;i<1000; i++ ) {
-            int t = (int) (Math.random() * 2 + 0);
-            int p =(int) (Math.random() * 20 + 60);
-            int l =(int) (Math.random() * 4 + 0);
-            bulb b = flyweight.getBulb(t,p,brandArray[l]);
-        }
-        long endTime = System.nanoTime();
-
-        long timeElapsed = endTime - startTime;
-        
-        System.out.println("Execution time for flyweight in milliseconds : " +
-                timeElapsed / 1000000);
-
-        long afterUsedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
-
-        long actualMemUsed=afterUsedMem-beforeUsedMem;
-
-        System.out.println("MEMORY USAGE IS: "+actualMemUsed);
+    Composite company = new Composite("Home Depot");
+    Composite oregon = new Composite("Oregon");
+    Leaf name1 = new Leaf("john");
+    Leaf name2 = new Leaf("james");
+    Leaf name3 = new Leaf("sally");
+    Leaf name4 = new Leaf("jake");
+    Leaf name5 = new Leaf("zack");
+    company.addMember(name1);
+    company.addMember(name5);
+    company.addMember(oregon);
+    oregon.addMember(name2);
+    oregon.addMember(name3);
+    company.addMember(name4);
 
 
+    company.print();
+
+        VisitorIF find = new Visitor();
+        System.out.println(company.accept(find, "john"));
+        System.out.println(company.accept(find, "xyz"));
     }
 }
