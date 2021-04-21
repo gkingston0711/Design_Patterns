49c86a3b74b08ca249643c740962ffc9db20b6e3
Joey Kingston <48415221+gkingston0711@users.noreply.github.com>
2021-04-21 09:44:50 -0700

    marker

---
+++ Marker/Marker.java
@@ -0,0 +1,35 @@
+package com.company;
+
+interface isOld{
+ //empty, there is nothing here due to pattern design
+}
+
+
+/**
+ *best site for marker
+ *https://www.baeldung.com/java-marker-interfaces
+ */
+
+class Car implements isOld{
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
